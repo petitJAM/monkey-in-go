dab4daad1e7abedeef5689cc43fef9cb8647813e
Alex Petitjean <alex.m.petitjean@gmail.com>
2024-05-03 23:09:08 -0500

    Add repl

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"monkey/repl"
+)
 
 func main() {
-	fmt.Println("hello monkey")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! It's Monkey time\n", user.Username)
+	fmt.Println("Type in some stuff")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
